518751f5dd5a4faae2df823fb9d9497f6087dc3b
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-07-24 18:21:44 +0300

    add props to App and edits to readMe

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Table from './Table'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <Table />
+class App extends Component {
+  render () {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+
+    return (
+      <div className="App">
+        <div className="container">
+          <Table characterData = {characters}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }  
 }
 
 export default App;
