96506fbeb1ffdda8354e45247a67ceb6b055fcd0
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-07-24 16:51:18 +0300

    add simple tableBody component

--- src/Table.js
+++ src/Table.js
@@ -11,6 +11,29 @@ const TableHeader = () => {
     )
 }
 
+const TableBody = () => {
+    return (
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
+    );
+}
+
 class Table extends Component {
     render() {
       return (
