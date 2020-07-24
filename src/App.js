f0f7972e671ebd7fd4b90e1707f6295674aca815
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-07-24 19:17:00 +0300

    add form component with initialState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    characters: []
   }
 
   removeCharacter = (index) => {
@@ -41,6 +25,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <Table characterData = {characters} removeCharacter = {this.removeCharacter}/>
+          <Form /> 
         </div>
       </div>
     );
